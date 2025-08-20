5a29136662a44e1494ac076282698df123513df8
kangkang shang <535634238@qq.com>
2025-08-20 21:14:24 +0800

    合并ssg 和普通app

--- src/main.ts
+++ src/main.ts
@@ -27,3 +27,31 @@ app.use(createRouter({
   routes,
 }))
 app.mount('#app')
+
+// SSG 方式启动
+// import type { UserModule } from './types'
+// import { ViteSSG } from 'vite-ssg'
+
+// import { routes } from 'vue-router/auto-routes'
+// import App from './App.vue'
+
+// import '~/styles/index.scss'
+
+// import 'uno.css'
+// If you want to use ElMessage, import it.
+// import 'element-plus/theme-chalk/src/message.scss'
+// import 'element-plus/theme-chalk/src/message-box.scss'
+
+// export const createApp = ViteSSG(
+//   App,
+//   {
+//     routes,
+//     base: import.meta.env.BASE_URL,
+//   },
+//   (ctx) => {
+//     // install all modules under `modules/`
+//     Object.values(import.meta.glob<{ install: UserModule }>('./modules/*.ts', { eager: true }))
+//       .forEach(i => i.install?.(ctx))
+//     // ctx.app.use(Previewer)
+//   },
+// )
