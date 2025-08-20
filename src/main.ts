9d228aa4cba6db1da44ec8cc37de426eb099fd6f
kangkang shang <535634238@qq.com>
2025-08-20 17:57:14 +0800

    调整代码

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,29 @@
-import type { UserModule } from './types'
-import { ViteSSG } from 'vite-ssg'
+import ElementPlus from 'element-plus'
+import { createApp } from 'vue'
 
-// import "~/styles/element/index.scss";
-
-// import ElementPlus from "element-plus";
-// import all element css, uncommented next line
-// import "element-plus/dist/index.css";
-
-// or use cdn, uncomment cdn link in `index.html`
-
+import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
+
 import App from './App.vue'
 
+// import all element css, uncommented next line
 import '~/styles/index.scss'
-
 import 'uno.css'
-// If you want to use ElMessage, import it.
+
+// unplugin-vue-components/vite 可以解决组件样式的自动引入
+// 但是无法解决组件作为 js api 调用时的样式引入
+// 这几个无法自动导入，单独引入
 import 'element-plus/theme-chalk/src/message.scss'
 import 'element-plus/theme-chalk/src/message-box.scss'
+import 'element-plus/theme-chalk/src/notification.scss'
 
-// if you do not need ssg:
-// import { createApp } from "vue";
+// 引入 element-plus 的全量 css
+// import 'element-plus/theme-chalk/src/index.scss'
 
-// const app = createApp(App);
-// app.use(createRouter({
-//   history: createWebHistory(),
-//   routes,
-// }))
-// // app.use(ElementPlus);
-// app.mount("#app");
-
-// https://github.com/antfu/vite-ssg
-export const createApp = ViteSSG(
-  App,
-  {
-    routes,
-    base: import.meta.env.BASE_URL,
-  },
-  (ctx) => {
-    // install all modules under `modules/`
-    Object.values(import.meta.glob<{ install: UserModule }>('./modules/*.ts', { eager: true }))
-      .forEach(i => i.install?.(ctx))
-    // ctx.app.use(Previewer)
-  },
-)
+const app = createApp(App)
+app.use(ElementPlus)
+app.use(createRouter({
+  history: createWebHistory(),
+  routes,
+}))
+app.mount('#app')
