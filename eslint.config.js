ac2d9264132941ec99e15a77cb4158c0a308ac5c
kangkang shang <535634238@qq.com>
2025-09-02 00:20:56 +0800

    chore(eslint): 更新 ESLint 配置添加 brace-style 和 html-self-closing 规则

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,19 @@ export default antfu({
   formatters: true,
   unocss: true,
   vue: true,
+  rules: {
+    'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+  },
 })
