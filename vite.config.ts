13825daa58eed7b6d0295c2b7cde8bd0335a53e4
kangkang shang <535634238@qq.com>
2025-09-02 01:03:42 +0800

    refactor(vite): 统一使用大写的 UnoCSS 导入名称
    将 unocss/vite 的导入名称从 Unocss 改为 UnoCSS，保持与官方文档和命名规范一致

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'node:path'
 import Vue from '@vitejs/plugin-vue'
 
-import Unocss from 'unocss/vite'
+import UnoCSS from 'unocss/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import Icons from 'unplugin-icons/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -59,7 +59,7 @@ export default defineConfig({
     }),
     // https://github.com/antfu/unocss
     // see uno.config.ts for config
-    Unocss(),
+    UnoCSS(),
   ],
   server: {
     proxy: {
