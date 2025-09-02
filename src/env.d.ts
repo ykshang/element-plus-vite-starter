7f48eafa82fe66e77cbb97de6c7160a4c51c5f29
kangkang shang <535634238@qq.com>
2025-09-02 14:34:17 +0800

    feat(图标): 添加自动安装图标集功能并调整插件顺序
    在 vite.config.ts 中新增 Icons 插件配置并调整插件顺序，同时在 env.d.ts 中添加图标模块的类型声明以支持自动安装的图标集

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,9 @@ declare module '*.vue' {
   const component: DefineComponent<object, object, any>
   export default component
 }
+declare module '~icons/*' {
+  import type { FunctionalComponent, SVGAttributes } from 'vue'
+
+  const component: FunctionalComponent<SVGAttributes>
+  export default component
+}
