dae04c697877d1681675216e3ce348371fe9ddce
kangkang shang <535634238@qq.com>
2025-09-02 17:05:43 +0800

    feat(字典管理): 添加表格加载状态以提高用户体验
    在字典管理页面添加表格加载状态指示器，防止数据加载期间用户误操作

--- src/pages/index/Setting/DictionaryManagent.vue
+++ src/pages/index/Setting/DictionaryManagent.vue
@@ -35,10 +35,13 @@ function onClickSearch() {
 // 字典列表
 const tableData = ref<Dictionary[]>([])
 
+const tableLoading = ref(false)
 // 获取查询结果
 function initTableData() {
+  tableLoading.value = true
   const requestParam = getRequestParam()
   dictionaryService.getDictionaryListByPage(requestParam).then((res: any) => {
+    tableLoading.value = false
     // console.log('字典列表', res)
     if (res.status === 'success') {
       tableData.value = res.result.list
@@ -86,7 +89,7 @@ function getRequestParam() {
         </el-form-item>
       </el-form>
     </div>
-    <div class="page-container-content flex flex-1 flex-col">
+    <div v-loading="tableLoading" class="page-container-content flex flex-1 flex-col">
       <el-table :data="tableData" style="width: 100%; height: 100%;">
         <el-table-column type="index" width="50" />
         <el-table-column prop="dictionaryKey" label="关键字" width="200" />
