c208c65967c8b54438feaf3f1afd1623620c960b
kangkang shang <535634238@qq.com>
2025-09-02 20:01:01 +0800

    fix: Add message-box‘ s overlay style - When used with ElTable, the z-index of message box does not take effect. Style conflict

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ import 'uno.css'
 // If you want to use ElMessage, import it.
 import 'element-plus/theme-chalk/src/message.scss'
 import 'element-plus/theme-chalk/src/message-box.scss'
+import 'element-plus/theme-chalk/src/overlay.scss' // the modal class for message box
 
 // if you do not need ssg:
 // import { createApp } from "vue";
