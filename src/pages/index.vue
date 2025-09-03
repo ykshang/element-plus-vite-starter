d1a34a392b00aaaa459bd9c56e2522d09f587a24
kangkang shang <535634238@qq.com>
2025-09-03 11:56:06 +0800

    fix(布局): 调整页面边距并移除冗余样式
    修复 Vue 警告信息，将 margin 属性移到外层容器，并统一调整页面边距为 10px

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -24,8 +24,8 @@ emitter.on('refreshPage', () => {
     <div class="main-container flex flex-1 flex-col overflow-hidden bg-[--ep-bg-color-page]">
       <base-layout-header />
       <!-- 把属性挪到外层，修复一些 Vue 警告信息 -->
-      <div class="mx-20px flex flex-1 flex-col overflow-x-hidden overflow-y-auto bg-[--ep-bg-color-page]">
-        <router-view :key="refreshKey" mt-20px />
+      <div class="mx-10px mt-10px flex flex-1 flex-col overflow-x-hidden overflow-y-auto bg-[--ep-bg-color-page]">
+        <router-view :key="refreshKey" />
         <base-layout-footer class="mt-2px min-h-80px bg-[--ep-bg-color]" />
       </div>
     </div>
