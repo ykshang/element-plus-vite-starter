50a31a10b476033e42dfb048e21c2557e3e85e4d
kangkang shang <535634238@qq.com>
2025-09-03 17:58:05 +0800

    feat(字典服务): 添加更新字典功能并优化注释
    新增 updateDictionary 方法用于更新字典，同时统一了 removeDictionary 方法中的注释描述

--- src/composables/services/dictionaryService.ts
+++ src/composables/services/dictionaryService.ts
@@ -16,10 +16,17 @@ export function createDictionary(params: any) {
     return Promise.resolve(res)
   })
 }
+export function updateDictionary(params: any) {
+  // console.log('更新字典参数', params)
+  return post('/api/dictionary/updateDictionary', params).then((res) => {
+    // console.log('更新字典', res)
+    return Promise.resolve(res)
+  })
+}
 export function removeDictionary(params: any) {
-  // console.log('创建字典参数', params)
+  // console.log('字典参数', params)
   return post('/api/dictionary/removeDictionary', params).then((res) => {
-    // console.log('创建字典成功', res)
+    // console.log('删除结果', res)
     return Promise.resolve(res)
   })
 }
