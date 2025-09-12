f9670e205b0f0ee10c6c9b64825447a92a3f0c8e
kangkang shang <535634238@qq.com>
2025-09-12 20:54:09 +0800

    refactor(echarts): 将渲染器从CanvasRenderer替换为SVGRenderer
    SVGRenderer提供更好的矢量图形支持，适用于需要缩放或打印的场景

--- src/config/echarts.ts
+++ src/config/echarts.ts
@@ -13,7 +13,7 @@ import * as echarts from 'echarts/core'
 // 标签自动布局、全局过渡动画等特性
 import { LabelLayout, UniversalTransition } from 'echarts/features'
 // 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
-import { CanvasRenderer } from 'echarts/renderers'
+import { SVGRenderer } from 'echarts/renderers'
 
 // 注册必须的组件
 echarts.use([
@@ -25,7 +25,7 @@ echarts.use([
   BarChart,
   LabelLayout,
   UniversalTransition,
-  CanvasRenderer,
+  SVGRenderer,
 ])
 
 export default echarts
