6a69abe9f47a283d682f98c6ac44d50dce399d65
kangkang shang <535634238@qq.com>
2025-09-12 20:50:24 +0800

    feat(DashBoard): 添加echarts图表展示数据趋势
    在仪表盘卡片中新增echarts图表功能，用于更直观地展示数据变化趋势。修改了数据计算逻辑并添加图表配置，移除原有简单数字展示方式。

--- src/views/home/DashBoard/dash-board-card-1.vue
+++ src/views/home/DashBoard/dash-board-card-1.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { ref, watchEffect } from 'vue'
+import { onMounted, ref } from 'vue'
+import echarts from '~/config/echarts'
 
 interface dataSourceType1 {
   date: string
@@ -12,12 +13,56 @@ interface Props {
 const props = defineProps<Props>()
 const totalNum = ref(0)
 const createdNum = ref(0)
-watchEffect(() => {
+const myChart = ref()
+const optionData = ref({
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow',
+    },
+  },
+  grid: {
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  xAxis: {
+    show: false,
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  },
+  yAxis: {
+    show: false,
+  },
+  series: [
+    {
+      data: [120, 200, 150, 80, 70, 110, 130],
+      type: 'bar',
+      itemStyle: {
+        color: 'rgb(121, 187, 255)',
+        barBorderRadius: 5,
+        barWidth: '50%',
+      },
+    },
+  ],
+})
+onMounted(() => {
+  // console.log(myChart.value)
   const dataSource = props.dataSource
+  const xAxisData: string[] = []
+  const seriesData: number[] = []
   dataSource.forEach((item) => {
     totalNum.value += item.createNum
+    xAxisData.push(item.date)
+    seriesData.push(item.createNum)
   })
   createdNum.value = dataSource[dataSource.length - 1].createNum
+
+  optionData.value.xAxis.data = xAxisData
+  optionData.value.series[0].data = seriesData
+  const mychart = echarts.init(myChart.value)
+  mychart.setOption(optionData.value)
 })
 </script>
 
@@ -31,11 +76,13 @@ watchEffect(() => {
         {{ totalNum }}
       </div>
       <div class="card-item-num2">
-        较昨日 <span class="color-red font-bold">{{ createdNum }}<div inline-flex font-size-10px class="i-ri:arrow-up-long-line" /></span>
+        较昨日 <span class="color-red font-bold">{{ createdNum }}
+          <div inline-flex font-size-10px class="i-ri:arrow-up-long-line" />
+        </span>
       </div>
     </div>
-    <div class="right">
-      1
+    <div class="mx-10px mt-10px flex flex-1">
+      <div ref="myChart" class="flex-1" />
     </div>
   </div>
 </template>
@@ -45,31 +92,33 @@ watchEffect(() => {
   height: 100px;
   padding: 15px;
   display: flex;
+
   .left {
     width: 110px;
     display: flex;
     flex-direction: column;
     color: var(--ep-text-color-regular);
+
     .card-item-title {
       font-size: 18px;
       font-weight: 600;
       margin-bottom: 10px;
     }
+
     .card-item-num1 {
       font-size: 26px;
       flex: 1;
       display: flex;
       align-items: center;
     }
+
     .card-item-num2 {
       font-size: 12px;
       color: var(--ep-text-color-regular);
     }
   }
-  .right {
-    flex: 1;
-  }
 }
+
 .card-primary {
   background: linear-gradient(var(--ep-color-primary-light-9), var(--ep-color-primary-light-8));
 }
