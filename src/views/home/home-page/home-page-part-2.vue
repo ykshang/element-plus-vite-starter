11d40ef679221667078ec8e3cd6afc72593b150c
kangkang shang <535634238@qq.com>
2025-09-13 20:38:42 +0800

    feat(首页): 更新待办事项状态显示和操作按钮
    - 修改状态值和对应的状态文本，简化状态选项
    - 为状态列添加标签样式，区分不同状态
    - 将操作按钮文本从"处理"改为"查看"

--- src/views/home/home-page/home-page-part-2.vue
+++ src/views/home/home-page/home-page-part-2.vue
@@ -15,13 +15,13 @@ const originData = ref<IOriginData[]>([
   {
     date: 1757764435124,
     title: '人事调动',
-    status: 3,
+    status: 0,
     detail: '关于湖北代表处李国胜人事调动',
   },
   {
     date: 1755664435124,
     title: '订单处理',
-    status: 2,
+    status: 0,
     detail: '北方科技公司-汽配零件订单 order-20230810001 ',
   },
   {
@@ -33,11 +33,11 @@ const originData = ref<IOriginData[]>([
   {
     date: 1755464435124,
     title: '设备采购',
-    status: 0,
+    status: 1,
     detail: '地区部办公室采购惠普打印机',
   },
 ])
-const statusText = ref(['待处理', '已审结', '出库中', '审批中'])
+const statusText = ref(['待处理', '已完成'])
 const tableData = ref<IOriginData[]>([])
 tableData.value = originData.value.map((item) => {
   item.dateLabel = dayjs(item.date).format('YYYY-MM-DD HH:mm:ss')
@@ -53,13 +53,19 @@ tableData.value = originData.value.map((item) => {
     </template>
     <el-table :data="tableData" :show-header="false" :border="false" height="230" :scrollbar-always-on="true" :stripe="true">
       <el-table-column prop="title" label="待办项" width="120" />
-      <el-table-column prop="statusText" show-overflow-tooltip label="状态" width="120" />
+      <el-table-column prop="statusText" show-overflow-tooltip label="状态" width="120">
+        <template #default="{ row }">
+          <el-tag :type="row.status === 0 ? 'primary' : 'info'">
+            {{ row.statusText }}
+          </el-tag>
+        </template>
+      </el-table-column>
       <el-table-column prop="detail" show-overflow-tooltip label="详情" />
       <el-table-column prop="dateLabel" label="日期" width="180" />
       <el-table-column prop="operation" show-overflow-tooltip label="操作" width="80">
         <template #default>
           <el-button type="text" width="60">
-            处理
+            查看
           </el-button>
         </template>
       </el-table-column>
