30aab2c511f49a3e97ec490e405692561f829447
kangkang shang <535634238@qq.com>
2025-09-14 23:30:08 +0800

    feat(用户管理): 添加办公地址选择表单功能
    - 在组件类型声明中添加 ElCascader 组件
    - 实现用户信息表单，包含姓名、性别、生日等字段验证
    - 添加办公地址级联选择器，使用预设的办公地点数据
    - 创建办公地点数据配置文件，包含国内外主要城市办公室信息

--- src/views/Setting/ProfileManagent/peofile-managent-right.vue
+++ src/views/Setting/ProfileManagent/peofile-managent-right.vue
@@ -1,5 +1,132 @@
+<!-- eslint-disable no-console -->
+<script lang="ts" setup>
+import type { FormInstance, FormRules } from 'element-plus'
+
+import type { UserInfoStore } from '~/types/User'
+import { storeToRefs } from 'pinia'
+import { reactive, ref } from 'vue'
+
+import officeData from '~/config/office-data'
+import { useUserInfoStore } from '~/stores/userIfnoStroe'
+
+const options = ref(officeData)
+
+const userInfoStore = useUserInfoStore()
+
+const { currentUserInfo } = storeToRefs(userInfoStore)
+
+const ruleForm = currentUserInfo
+
+const ruleFormRef = ref<FormInstance>()
+
+const rules = reactive<FormRules<UserInfoStore>>({
+  lastName: [
+    { required: true, message: '请输入姓氏', trigger: 'blur' },
+    { min: 1, max: 50, message: '长度不能超过50个字符', trigger: 'blur' },
+  ],
+  firstName: [
+    { required: true, message: '请输入名字', trigger: 'blur' },
+    { min: 1, max: 50, message: '长度不能超过50个字符', trigger: 'blur' },
+  ],
+  fullName: [
+    { required: true, message: '请输入姓名', trigger: 'blur' },
+    { min: 1, max: 100, message: '长度不能超过100个字符', trigger: 'blur' },
+  ],
+  sex: [
+    {
+      required: true,
+      message: '请选择性别',
+      trigger: 'change',
+    },
+  ],
+  phoneNumber: [
+    { required: true, message: '请输入手机号', trigger: 'blur' },
+    { min: 11, max: 11, message: '手机号长度必须为11位', trigger: 'blur' },
+    { pattern: /^1[3-9]\d{9}$/, message: '请输入正确的手机号', trigger: 'blur' },
+  ],
+  birthday: [
+    {
+      type: 'date',
+      required: true,
+      message: '请选择生日',
+      trigger: 'change',
+    },
+  ],
+  offices: [
+    {
+      required: true,
+      message: '请选择办公地址',
+      trigger: 'change',
+    },
+  ],
+})
+
+async function submitForm(formEl: FormInstance | undefined) {
+  if (!formEl)
+    return
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      console.log('submit!')
+    } else {
+      console.log('error submit!', fields)
+    }
+  })
+}
+</script>
+
 <template>
   <el-card shadow="never">
-    1
+    <el-form
+      ref="ruleFormRef"
+      style="max-width: 600px"
+      :model="ruleForm"
+      :rules="rules"
+      label-width="150"
+      label-position="left"
+      label-suffix="："
+    >
+      <el-form-item label="姓氏" prop="lastName" required>
+        <el-input v-model="ruleForm.lastName" placeholder="请输入姓氏" />
+      </el-form-item>
+      <el-form-item label="名字" prop="firstName" required>
+        <el-input v-model="ruleForm.firstName" placeholder="请输入名字" />
+      </el-form-item>
+      <el-form-item label="姓名" prop="fullName" required>
+        <el-input v-model="ruleForm.fullName" placeholder="请输入姓名" />
+      </el-form-item>
+      <el-form-item label="性别" prop="sex" required>
+        <el-radio-group v-model="ruleForm.sex">
+          <el-radio value="男">
+            男
+          </el-radio>
+          <el-radio value="女">
+            女
+          </el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="生日" prop="birthday" required>
+        <el-date-picker
+          v-model="ruleForm.birthday"
+          type="date"
+          aria-label="请选择生日"
+          placeholder="请选择生日"
+          style="width: 100%"
+        />
+      </el-form-item>
+      <el-form-item label="手机号" prop="phoneNumber" required>
+        <el-input v-model="ruleForm.phoneNumber" placeholder="请输入手机号" />
+      </el-form-item>
+      <el-form-item label="办公地址" prop="offices" required>
+        <el-cascader v-model="ruleForm.offices" style="flex:1" :options="options" />
+      </el-form-item>
+      <el-form-item label="简介" prop="brief">
+        <el-input v-model="ruleForm.brief" type="textarea" placeholder="请输入简介" :autosize="{ minRows: 4, maxRows: 6 }" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm(ruleFormRef)">
+          保存
+        </el-button>
+      </el-form-item>
+    </el-form>
   </el-card>
 </template>
