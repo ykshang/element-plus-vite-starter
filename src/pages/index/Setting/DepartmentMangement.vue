bcc156915148a4b390e17efd09e0bc38c4fbc5e9
kangkang shang <535634238@qq.com>
2025-09-15 10:59:48 +0800

    feat(DepartmentMangement): 添加部门管理左右分栏布局
    引入 Element Plus 的 ElSplitter 组件实现部门管理页面的左右分栏布局
    新增 department-mangement-left 和 department-mangement-right 组件
    更新组件类型声明文件以支持新组件和 ElSplitter 相关类型

--- src/pages/index/Setting/DepartmentMangement.vue
+++ src/pages/index/Setting/DepartmentMangement.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>DepartmentMangement</div>
+  <el-splitter>
+    <el-splitter-panel size="30%">
+      <div class="demo-panel">
+        1
+      </div>
+    </el-splitter-panel>
+    <el-splitter-panel :min="200">
+      <div class="demo-panel">
+        2
+      </div>
+    </el-splitter-panel>
+  </el-splitter>
 </template>
