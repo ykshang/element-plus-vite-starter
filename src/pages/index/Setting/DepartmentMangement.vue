ddb2b7884c92615f2e6dd4af654be76908d3d6fd
kangkang shang <535634238@qq.com>
2025-09-15 19:39:01 +0800

    refactor(DepartmentManagement): 移除硬编码的用户表格数据
    硬编码的测试数据已从组件中移除，为后续从API获取真实数据做准备

--- src/pages/index/Setting/DepartmentMangement.vue
+++ src/pages/index/Setting/DepartmentMangement.vue
@@ -30,47 +30,6 @@ function load(row: User, treeNode: unknown, resolve: (data: User[]) => void) {
   }, 1000)
 }
 
-const tableData: User[] = [
-  {
-    id: 1,
-    date: '2016-05-02',
-    name: 'wangxiaohu',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    id: 2,
-    date: '2016-05-04',
-    name: 'wangxiaohu',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    id: 3,
-    date: '2016-05-01',
-    name: 'wangxiaohu',
-    address: 'No. 189, Grove St, Los Angeles',
-    children: [
-      {
-        id: 31,
-        date: '2016-05-01',
-        name: 'wangxiaohu',
-        address: 'No. 189, Grove St, Los Angeles',
-      },
-      {
-        id: 32,
-        date: '2016-05-01',
-        name: 'wangxiaohu',
-        address: 'No. 189, Grove St, Los Angeles',
-      },
-    ],
-  },
-  {
-    id: 4,
-    date: '2016-05-03',
-    name: 'wangxiaohu',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-]
-
 const tableData1: User[] = [
   {
     id: 1,
