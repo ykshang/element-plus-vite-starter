fc95f9bcfe917929a74206b8d68d88a3121ab27d
kangkang shang <535634238@qq.com>
2025-09-15 21:37:20 +0800

    feat(部门管理): 实现部门列表展示及新增功能
    - 添加部门服务调用获取部门数据
    - 实现部门表格展示及格式化时间
    - 添加创建部门弹窗功能
    - 移除静态测试数据，使用真实接口数据

--- src/pages/index/Setting/DepartmentMangement.vue
+++ src/pages/index/Setting/DepartmentMangement.vue
@@ -1,7 +1,46 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import dayjs from 'dayjs'
+import { nextTick, onMounted, ref } from 'vue'
+import departmentService from '~/composables/services/departmentService'
 
 const departmentName = ref('')
+const tableData = ref([])
+const showAddDepartmentFlg = ref(false)
+const addDepartmentRef = ref()
+
+showAddDepartmentFlg.value = true
+
+function openAddDepartment() {
+  showAddDepartmentFlg.value = true
+  nextTick(() => {
+  // console.log('AddDictionaryRef', addDepartmentRef.value)
+    addDepartmentRef.value.handleOpen()
+  })
+}
+function closeAddDepartment(refreshFlg: string) {
+  showAddDepartmentFlg.value = false
+  if (refreshFlg === 'refresh') {
+    getTableData()
+  }
+}
+
+function getTableData() {
+  departmentService.main({
+    departmentName: departmentName.value,
+  }).then((res: any) => {
+    if (res.success) {
+      tableData.value = res.result.map((item: any) => {
+        item.hasChildren = true
+        item.updatedAtLabel = dayjs(item.updatedAt).format('YYYY-MM-DD HH:mm:ss')
+        item.createdAtLabel = dayjs(item.createdAt).format('YYYY-MM-DD HH:mm:ss')
+        return item
+      })
+    }
+  })
+}
+onMounted(() => {
+  getTableData()
+})
 interface User {
   id: number
   date: string
@@ -29,60 +68,43 @@ function load(row: User, treeNode: unknown, resolve: (data: User[]) => void) {
     ])
   }, 1000)
 }
-
-const tableData1: User[] = [
-  {
-    id: 1,
-    date: '2016-05-02',
-    name: 'wangxiaohu',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    id: 2,
-    date: '2016-05-04',
-    name: 'wangxiaohu',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    id: 3,
-    date: '2016-05-01',
-    name: 'wangxiaohu',
-    hasChildren: true,
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    id: 4,
-    date: '2016-05-03',
-    name: 'wangxiaohu',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-]
 </script>
 
 <template>
   <el-card shadow="never" mb-10px flex-1>
     <div mb-15px flex>
-      <el-button type="primary">
+      <el-button type="primary" @click="openAddDepartment">
         创建一级部门
       </el-button>
       <div ml-a flex>
         <el-input v-model="departmentName" placeholder="请输入部门名称" clearable style="width: 300px" />
-        <el-button type="primary" ml-10px w-40px>
+        <el-button type="primary" ml-10px w-40px @click="getTableData">
           查询
         </el-button>
       </div>
     </div>
     <el-table
-      :data="tableData1"
-      style="width: 100%"
-      row-key="id"
-      lazy border
-      :load="load"
+      :data="tableData" style="width: 100%" row-key="_id" lazy :load="load"
       :tree-props="{ children: 'children', hasChildren: 'hasChildren' }"
     >
-      <el-table-column prop="date" label="Date" />
-      <el-table-column prop="name" label="Name" />
-      <el-table-column prop="address" label="Address" />
+      <el-table-column prop="departmentCode" label="部门编码" />
+      <el-table-column prop="departmentName" label="部门名称" width="260" />
+      <el-table-column prop="departmentLevel" label="部门层级" width="120" />
+      <el-table-column prop="parentDepartmentCode" label="父级部门编码" width="220" />
+      <el-table-column prop="description" label="描述" />
+      <el-table-column prop="createdAtLabel" label="创建时间" width="180" />
+      <el-table-column prop="updatedAtLabel" label="更新时间" width="180" />
+      <el-table-column prop="operation" label="操作" width="150">
+        <template #default>
+          <el-button link type="primary">
+            编辑
+          </el-button>
+          <el-button link type="danger">
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
     </el-table>
+    <department-mangement-add v-if="showAddDepartmentFlg" ref="addDepartmentRef" @close="closeAddDepartment" />
   </el-card>
 </template>
