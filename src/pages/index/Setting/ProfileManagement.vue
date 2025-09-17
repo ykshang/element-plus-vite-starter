2cb6a730a28e44734590a312762621f9dfc4a45b
kangkang shang <535634238@qq.com>
2025-09-17 12:46:56 +0800

    fix(profile): 修正组件名称拼写错误并添加新组件
    修复ProfileManagement.vue中组件名称的拼写错误，将"peofile-managent"更正为"profile-management"
    同时在components.d.ts中添加新的组件类型声明
    新增profile-management-left和profile-management-right组件实现用户信息展示和编辑功能

--- src/pages/index/Setting/ProfileManagement.vue
+++ src/pages/index/Setting/ProfileManagement.vue
@@ -1,10 +1,10 @@
 <template>
   <el-row flex-1 :gutter="10">
     <el-col :span="10">
-      <peofile-managent-left />
+      <profile-management-left />
     </el-col>
     <el-col :span="14">
-      <peofile-managent-right />
+      <profile-management-right />
     </el-col>
   </el-row>
 </template>
