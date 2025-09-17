4332da36977b036eaecfbf7d5104098ffc489ddb
kangkang shang <535634238@qq.com>
2025-09-17 21:01:28 +0800

    feat(图表列表): 实现图表卡片组件并添加间距样式
    - 在ChartList.vue中添加12个图表卡片组件
    - 为图表列表容器添加底部边距和间距
    - 实现chart-list-card组件，包含ECharts折线图配置
    - 使用Vue组合式API初始化图表并设置响应式数据

--- src/pages/index/Components/ChartList.vue
+++ src/pages/index/Components/ChartList.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="grid grid-cols-4">
+  <div class="grid grid-cols-4 mb-10px gap-10px">
     <chart-list-card-1 />
-    <div>1</div>
-    <div>1</div>
-    <div>1</div>
+    <chart-list-card-2 />
+    <chart-list-card-3 />
+    <chart-list-card-4 />
+    <chart-list-card-5 />
+    <chart-list-card-6 />
+    <chart-list-card-7 />
+    <chart-list-card-8 />
+    <chart-list-card-9 />
+    <chart-list-card-10 />
+    <chart-list-card-11 />
+    <chart-list-card-12 />
   </div>
 </template>
--- src/views/Components/ChartList/chart-list-card-4.vue
+++ src/views/Components/ChartList/chart-list-card-4.vue
@@ -1,5 +1,37 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import echarts from '~/config/echarts'
+
+const optionData = ref({
+  grid: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+  },
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  },
+  yAxis: {
+    type: 'value',
+  },
+  series: [
+    {
+      data: [150, 230, 224, 218, 135, 147, 260],
+      type: 'line',
+    },
+  ],
+})
+const myChart = ref()
+onMounted(() => {
+  const mychart = echarts.init(myChart.value)
+  mychart.setOption(optionData.value)
+})
+</script>
+
 <template>
-  <el-card>
-    2
+  <el-card header="折线图" h-300px flex flex-col body-class="flex flex-1">
+    <div ref="myChart" class="flex-1" />
   </el-card>
 </template>
