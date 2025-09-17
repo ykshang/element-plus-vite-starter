11d59df1262e6579023c7de3bfc5e2e33faf10bf
kangkang shang <535634238@qq.com>
2025-09-17 23:50:28 +0800

    feat: 添加水印组件，重新调整原来布局，配置z-index为999999 保证水印悬浮于所有组件上层

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,8 +2,9 @@
 import { ElNotification } from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import NProgress from 'nprogress'
-import { ref } from 'vue'
+import { reactive, ref, watch } from 'vue'
 import emitter from '~/config/emitter'
+import { isDark } from '~/config/toggleDarkTheme'
 
 const refreshKey = ref(0)
 
@@ -17,18 +18,37 @@ emitter.on('refreshPage', () => {
   refreshKey.value++
   NProgress.done() // 手动完成进度条
 })
+// 水印字体
+const font = reactive({
+  color: 'rgba(0, 0, 0, .15)',
+})
+// 水印文字颜色
+watch(
+  isDark,
+  (newVal) => {
+    // console.log('isDark', newVal)
+    font.color = newVal
+      ? 'rgba(255, 255, 255, .15)'
+      : 'rgba(0, 0, 0, .15)'
+  },
+  {
+    immediate: true,
+  },
+)
 </script>
 
 <template>
   <el-config-provider namespace="ep" :locale="zhCn">
-    <base-layout-side />
-    <div class="main-container flex flex-1 flex-col overflow-hidden bg-[--ep-bg-color-page]">
-      <base-layout-header />
-      <!-- 把属性挪到外层，修复一些 Vue 警告信息 -->
-      <div class="mx-10px mt-10px flex flex-1 flex-col overflow-x-hidden overflow-y-auto bg-[--ep-bg-color-page]">
-        <router-view :key="refreshKey" />
-        <base-layout-footer class="mt-2px min-h-80px bg-[--ep-bg-color]" />
+    <el-watermark :font="font" :z-index="999999" h-full w-full flex>
+      <base-layout-side />
+      <div class="main-container flex flex-1 flex-col overflow-hidden bg-[--ep-bg-color-page]">
+        <base-layout-header />
+        <!-- 把属性挪到外层，修复一些 Vue 警告信息 -->
+        <div class="mx-10px mt-10px flex flex-1 flex-col overflow-x-hidden overflow-y-auto bg-[--ep-bg-color-page]">
+          <router-view :key="refreshKey" />
+          <base-layout-footer class="mt-2px min-h-80px bg-[--ep-bg-color]" />
+        </div>
       </div>
-    </div>
+    </el-watermark>
   </el-config-provider>
 </template>
