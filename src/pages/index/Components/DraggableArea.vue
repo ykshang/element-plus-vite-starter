196cd0dcebcd26079248cd50098b42a0007ec720
kangkang shang <535634238@qq.com>
2025-09-18 19:10:55 +0800

    refactor(DraggableArea): 重构拖拽区域布局并增加项目数量
    将静态的9个项目改为动态生成40个项目
    使用Tailwind CSS类替代自定义网格布局
    添加user-select样式防止文本被选中

--- src/pages/index/Components/DraggableArea.vue
+++ src/pages/index/Components/DraggableArea.vue
@@ -2,30 +2,23 @@
 import { ref } from 'vue'
 import draggable from 'vuedraggable'
 
-const gridList = ref([
-  { id: 1, num: 1 },
-  { id: 2, num: 2 },
-  { id: 3, num: 3 },
-  { id: 4, num: 4 },
-  { id: 5, num: 5 },
-  { id: 6, num: 6 },
-  { id: 7, num: 7 },
-  { id: 8, num: 8 },
-  { id: 9, num: 9 },
-])
+const gridList = ref<{ id: number, num: number }[]>([])
+for (let i = 0; i < 40; i++) {
+  gridList.value.push({ id: i + 1, num: i + 1 })
+}
 </script>
 
 <template>
   <draggable
     v-model="gridList"
-    class="card grid-container"
+    class="card grid grid-cols-8 grid-rows-5 flex-1 gap-10px"
     item-key="id"
     animation="300"
     chosen-class="chosen"
     force-fallback="true"
   >
     <template #item="{ element }">
-      <div :class="`item` + ` ` + `item-${element.num}`">
+      <div :class="`item` + ` ` + `item-${element.num % 9 + 1}`">
         {{ element.num }}
       </div>
     </template>
@@ -33,21 +26,16 @@ const gridList = ref([
 </template>
 
 <style scoped lang="scss">
-.grid-container {
-  display: grid;
-  grid-template-rows: 33.3% 33.3% 33.3%;
-  grid-template-columns: 33.3% 33.3% 33.3%;
-  height: 100%;
-  .item {
-    box-sizing: border-box;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 25px;
-    color: #ffffff;
-    cursor: move;
-    border: 1px solid #e5e4e9;
-  }
+.item {
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 25px;
+  color: #ffffff;
+  cursor: move;
+  border: 1px solid #e5e4e9;
+  user-select: none;
 }
 .item-1 {
   background-color: #87bba2;
